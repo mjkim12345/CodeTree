14ea9ba9050e4d427bb09322fbc690f463b2b823
mjkim12345 <144995949+mjkim12345@users.noreply.github.com>
2025-03-31 17:06:27 +0900

    Title: 순서쌍 만들기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250331/순서쌍 만들기/making-order-pair.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    for (int i=N; i>0; i--) {
+        for (int j=N; j>0; j-- ) {
+            cout << "(" << i << "," << j << ")" << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
